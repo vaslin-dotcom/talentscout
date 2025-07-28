06aadfe3efa2c906920e721f35e7dc1e078806c7
vaslin-dotcom <antonyvaslin@gmail.com>
2025-07-28 15:18:28 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,6 @@
-import re
+from candidate_state import candidate_info
 
-def split_question_response(response: str):
-    compliment = ""
-    question = ""
-
-    for line in response.strip().split('\n'):
-        if line.startswith("Compliment:"):
-            compliment = line.replace("Compliment:", "").strip()
-        elif line.startswith("Question:"):
-            question = line.replace("Question:", "").strip()
-
-    return compliment, question
-
-def safe_filename(name):
-    return re.sub(r'\W+', '_', name)
+def update_candidate_info(parsed_data):
+    for key in candidate_info:
+        if key in parsed_data and candidate_info[key] is None:
+            candidate_info[key] = parsed_data[key]
