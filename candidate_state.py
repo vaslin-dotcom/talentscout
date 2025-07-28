36123199c08651bcf8ea29f87d0dc522ce836818
vaslin-dotcom <antonyvaslin@gmail.com>
2025-07-28 22:34:04 +0530

    Delete candidate_state.py

--- candidate_state.py
+++
@@ -1,8 +0,0 @@
-required_fields = [
-    "name", "email", "phone", "experience",
-    "desired_position", "location", "tech_stack"
-]
-
-candidate_info = {field: None for field in required_fields}
-candidate_info["responses"] = []
-candidate_info["proficiency_summary"] = None
