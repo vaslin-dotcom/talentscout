241cdfe6f53a20ebecc026cd6b7d30f9be4dfb17
vaslin-dotcom <antonyvaslin@gmail.com>
2025-07-28 22:35:08 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-from candidate_state import candidate_info
-
-def update_candidate_info(parsed_data):
-    for key in candidate_info:
-        if key in parsed_data and candidate_info[key] is None:
-            candidate_info[key] = parsed_data[key]
